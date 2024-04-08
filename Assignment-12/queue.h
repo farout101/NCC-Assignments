0353f6f332de6ed11cbffe6355c44d8302673105
Far Out <phyozawlinn1852020@gmail.com>
2024-04-08 17:34:58 +0630

    Update queue.h

--- Assignment-12/queue.h
+++ Assignment-12/queue.h
@@ -1,1 +1,21 @@
+#ifndef QUEUE_H
+#define QUEUE_H
 
+struct QueueNode {
+    int data;
+    struct QueueNode *next;
+};
+
+struct Queue {
+    struct QueueNode *front, *rear;
+};
+
+struct QueueNode *newNode(int data);
+struct Queue *createQueue();
+int isEmptyQueue(struct Queue *queue);
+void enqueue(struct Queue *queue, int data);
+int dequeue(struct Queue *queue);
+void emptyQueue(struct Queue *queue);
+void displayQueue(struct Queue *queue);
+
+#endif
